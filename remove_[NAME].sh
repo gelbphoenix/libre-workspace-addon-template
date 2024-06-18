ecdd38cb45a85c675f9e566ca54c96323780a4ae
Phoenix Paulina Schmid <122753910+gelbphoenix@users.noreply.github.com>
2024-06-18 19:44:39 +0200

    Added setup, update and remove script templates

---
+++ remove_[NAME].sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script gets three variables passed: $DOMAIN, $ADMIN_PASSWORD, $IP, $LDAP_DC
+
+# Place here the removal script for your addon. Refer to the documentation of Libre Workspace for details.
---
+++ update_[NAME].sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+# Place here the update script for your addon. Refer to the documentation of Libre Workspace for details.
